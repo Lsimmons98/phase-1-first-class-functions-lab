f9b9b82c96cdc28e2d3ad9b3520e22f02fcd18f6
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-07 10:47:34 -0400

    Add driver selection and fare multiplier functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers){
+    return arrayOfDrivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(arrayOfDrivers){
+    return arrayOfDrivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    function fareMultiplier(fare){
+        return fare * multiplier}
+    return fareMultiplier
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(arrayOfDrivers, returnDriversFunction){
+    return returnDriversFunction(arrayOfDrivers);
+}
